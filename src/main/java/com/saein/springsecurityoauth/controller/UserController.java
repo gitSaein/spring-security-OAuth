2c3c1d4de22c53520e676c549b3a94d29add3844
saein.lee <gitSaein@gmail.com>
2019-06-09 20:25:25 +0900

    spring security logout

---
+++ src/main/java/com/saein/springsecurityoauth/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.saein.springsecurityoauth.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+	@GetMapping(value = "/secure/logout")
+	public String logout(HttpServletRequest req, HttpServletResponse res) {
+		Authentication auth = 
+				SecurityContextHolder.getContext().getAuthentication();
+		if( auth != null ) {
+			new SecurityContextLogoutHandler()
+			.logout(req, res, auth);
+			req.getSession().invalidate();
+		}
+		return "redirect:/secure/welcome";
+	}
+}
